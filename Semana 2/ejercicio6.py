8cf06065cc509d47e5a86a172c9b901cc6b9534b
IsraelArbona <i.arbonammisena.edu.co>
2021-05-17 10:20:29 -0500

    organizar semana 3

---
+++ Semana 2/ejercicio6.py
@@ -0,0 +1,52 @@
+letra = 'fresa'
+letra = letra[1]
+# print(letra)
+
+letra = 'banano'
+# letra = letra[0]
+# print(letra)
+pos = len(letra)
+ultima = letra[pos-1]
+# print(ultima)
+
+s = 'Monty Python'
+#    0 1 2 3 4 5 6 7 8 9 10 11
+
+
+'''
+print(s[3:11]) - ty P 
+print(s[6:11])
+print(s[:2])
+print(s[2:])
+'''
+
+'''
+saludo = 'buenos dias'
+# saludo[0] = 'J'
+saludo = 'J' + saludo[1:]
+print(saludo)
+print('alo' in 'banana')
+
+
+A = 65
+B = 66
+b = 98
+
+palabra = 'banana'
+if palabra < 'banana':
+    print('palabra es menor')
+
+# print(dir(palabra))
+print(palabra.upper())
+
+index = palabra.find('na',5)
+print(index)
+'''
+
+# linea = '          Aquí vamos iniciar        '
+# print(linea.strip())
+
+camellos = 42
+
+print('%d' % camellos)
+print('he visto %d camellos' % camellos)
---
+++ Semana 3/ejercicio1.py
@@ -0,0 +1,46 @@
+''' 
+  contador = contador + 1
+  acumulador = acumulador + variable
+'''
+
+
+lista = [1, 2.5, "Devcode", [5,6], 4]
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+# print(lista[4])
+print(lista[3][0])
+print(lista[3][1])
+print('Hicimos este cambio ->', lista[1:4])
+print(lista[1:6])
+print(lista[1:5:2])
+
+
+'''
+nombre = 'Pepe'
+edad = 25
+lista = [nombre,edad]
+print(lista)
+nombre += 'Juan'
+print(lista)
+print(type(lista[0]))
+'''
+
+'''
+nombres = ['Juan', 'Luis']
+edades = [24,21]
+lista = [nombres,edades]
+print(lista)
+
+nombres += ['Cristina']
+
+print(nombres)
+print(type(lista[0]))
+'''
+
+'''
+versiones_iphone = [5,6,7,8]
+versiones_iphone.append(6)
+print(versiones_iphone)
+'''
